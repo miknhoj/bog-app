5242743eac25c48dc84f7a8cd8ba224208c616cb
miknhoj <kimjohan86@gmail.com>
2018-10-05 14:55:57 -0400

    get creature by Id working

--- bog-express-one/routes/creaturesController.js
+++ bog-express-one/routes/creaturesController.js
@@ -1,10 +1,16 @@
 const router = require('express').Router();
 const { Creature } = require('../db/models')
 
-/* GET home page. */
+// SHOW ALL
 router.get('/', async (req, res) => {
   const creatures = await Creature.find({})
     res.send(creatures)
+})
+
+// SHOW ONE
+router.get('/:id', async (req, res) => {
+  const creature = await Creature.findById(req.params.id)
+  res.send(creature)
 })
 
 
