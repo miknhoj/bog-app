eb28c9b9a1cd654b7876e1e424aa8f7ebcaa9b1e
miknhoj <kimjohan86@gmail.com>
2018-10-05 11:53:00 -0400

    installed mongoose

---
+++ bog-express-one/server.js
@@ -0,0 +1,33 @@
+require('dotenv').config() 
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+// CONNECTING DATABASE
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+const connection = mongoose.connection;
+connection.on('connected', () => {
+  console.log('Mongoose Connected Successfully');
+});
+// If the connection throws an error
+connection.on('error', (err) => {
+  console.log('Mongoose default connection error: ' + err);
+});
+
+const indexRouter = require('./routes');
+const usersRouter = require('./routes/users');
+
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
